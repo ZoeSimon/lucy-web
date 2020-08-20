378e80b711a9d3252ad18dd2a80a9dc373dd330a
Nick Phura <nickphura@gmail.com>
2020-08-20 10:51:36 -0700

    778: temp - update knexfile

--- api-mobile/knexfile.ts
+++ api-mobile/knexfile.ts
@@ -4,15 +4,46 @@ import dotenv from 'dotenv';
 
 const env = process.env.NODE_ENV || 'development';
 
+console.log('1===================================================');
+console.log('process.env:', process.env);
+console.log('2===================================================');
+console.log(env);
 if (env === 'development') {
-  dotenv.config();
+  const temp = dotenv.config();
+  console.log('dotenv:', JSON.stringify(temp.parsed));
 }
+console.log('3===================================================');
 
 module.exports = {
+  development: {
+    debug: true,
+    client: 'postgresql',
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT || 5432,
+      database: process.env.DB_DATABASE,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      // stub: './config/knex-migration-stub.js',
+      tableName: 'migration',
+      directory: 'src/database/migrations'
+    }
+    // seeds: {
+    //   // stub: './config/knex-migration-stub.js',
+    //   tableName: 'seed',
+    //   directory: 'src/database/seeds'
+    // }
+  },
   test: {
     client: 'postgresql',
     connection: {
-      host: process.env.DB_HOST_FOR_LOCAL_API,
+      host: process.env.DB_HOST,
       port: process.env.DB_PORT || 5432,
       database: process.env.DB_DATABASE,
       user: process.env.DB_USER,
@@ -28,35 +59,10 @@ module.exports = {
       directory: 'src/database/migrations'
     }
   },
-  development: {
-    client: 'postgresql',
-    connection: {
-      host: process.env.DB_HOST_FOR_LOCAL_API,
-      port: process.env.DB_PORT || 5432,
-      database: process.env.DB_DATABASE,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      // stub: './config/knex-migration-stub.js',
-      tableName: 'migration',
-      directory: 'src/database/migrations'
-    },
-    seeds: {
-      // stub: './config/knex-migration-stub.js',
-      tableName: 'seed',
-      directory: 'src/database/seeds'
-    }
-  },
-
   production: {
     client: 'postgresql',
     connection: {
-      host: process.env.DB_HOST_FOR_LOCAL_API,
+      host: process.env.DB_HOST,
       port: process.env.DB_PORT || 5432,
       database: process.env.DB_DATABASE,
       user: process.env.DB_USER,
