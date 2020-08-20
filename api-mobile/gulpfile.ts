140874f0026192394a22062aa3b9ce6ccc8bef57
Nick Phura <nickphura@gmail.com>
2020-08-19 18:07:32 -0700

    778: update gulpfile and package.json gulp script

--- api-mobile/gulpfile.ts
+++ api-mobile/gulpfile.ts
@@ -2,23 +2,17 @@ import del from 'del';
 import gulp from 'gulp';
 import ts from 'gulp-typescript';
 
+// Clean the dist folder, and build the api dist
+gulp.task('build', gulp.series('clean', 'ts-server'));
+
 // Clean (delete) the dist folder
 gulp.task('clean', () => {
   return del(['./dist']);
 });
 
 // Compile the api and create the dist folder
-const tsProject = ts.createProject('tsconfig.json');
 gulp.task('ts-server', () => {
+  const tsProject = ts.createProject('tsconfig.json');
   const tsResult = tsProject.src().pipe(tsProject());
   return tsResult.js.pipe(gulp.dest('dist'));
-});
-
-// Default `gulp` command
-// Clean the dist folder, and run the api
-gulp.task('default', gulp.series('clean', 'ts-server'));
-
-// Clean the dist folder, and run the api in watch mode
-gulp.task('watch', () => {
-  gulp.watch(['./**/*.ts'], gulp.series('clean', 'ts-server'));
 });
